a4959c148b743a0b276614ad1492cc5379d46edf
Vivek Jain <vivek@dewsolutions.in>
2024-10-21 16:49:28 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -49,134 +49,208 @@ app.get("/scrape", async (req, res) => {
     // await page.screenshot({ path: "digimon-website.png", fullPage: true });
 
     const result = {
-        team1Name: "",
-        team2Name: "",
-        overNum: "",
-        score: "",
-        crr: "",
-        reqrr: "",
+        team1Name: "Team 1",
+        team2Name: "Team 2",
+        overNum: "0.0",
+        score: "0/0",
+        crr: "0.00",
+        reqrr: "0.00",
         batters: [],
-        bowlers: [],
+        bowlers: [{ name: "NA", score: 0 }],
         recentBalls: [],
         currentBowler: "",
         isSecondInning: false,
         matchEnded: "",
+        matchEndedStr: "",
         error: false,
     };
 
-    try {
-        result.team1Name = await page.$eval(".team.clearfix.team1 b.text-uppercase", (el) => el.innerHTML);
-    } catch (error) {
-        result.error = true;
-        console.log(error);
-    }
-    try {
-        result.team2Name = await page.$eval(".team.clearfix.team2 b.text-uppercase", (el) => el.innerHTML);
-    } catch (error) {
-        result.error = true;
-        console.log(error);
-    }
-    try {
-        const overNum = await page.$eval(".end-of-over .end-title.text-uppercase", (el) => el.innerHTML);
-        const overNumSplit = overNum.split(" ");
-        result.overNum = overNumSplit[overNumSplit.length - 1];
-    } catch (error) {
-        result.error = true;
-        console.log(error);
-    }
-    try {
-        result.score = await page.$eval(".end-of-over .score", (el) => el.innerHTML);
-    } catch (error) {
-        result.error = true;
-        console.log(error);
-    }
-    try {
-        result.crr = await page.$eval(".rr.current-rr b", (el) => el.innerHTML);
-    } catch (error) {
-        result.error = true;
-        console.log(error);
-    }
-    try {
-        result.reqrr = await page.$eval(".rr.req-rr b", (el) => el.innerHTML);
-    } catch (error) {
-        result.error = true;
-        console.log(error);
-    }
+    // try {
+    //     result.team1Name = await page.$eval(".team.clearfix.team1 b.text-uppercase", (el) => el.innerHTML);
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
+    // try {
+    //     result.team2Name = await page.$eval(".team.clearfix.team2 b.text-uppercase", (el) => el.innerHTML);
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
+
+    // Teams names
+    // try {
+    //     const teamString = await page.$eval("h1.d-none", (el) => el.innerHTML);
+    //     const teams = teamString.replace("Cricket Match Scorecard", "").trim().split(" vs ");
+    //     if (teams.length === 2) {
+    //         result.team1Name = teams[0];
+    //         result.team2Name = teams[1];
+    //     }
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
 
     try {
-        const battersData = await page.$$eval(".end-of-over .bat-score span", (elements) => elements.map((e) => e.textContent).slice(0, 4));
-        let i = 1;
-        let data = {};
-        battersData.forEach((d) => {
-            if (i % 2 === 0) {
-                data.score = d;
-                result.batters.push(data);
+        const pageData = await page.$$eval("script", (scripts) => JSON.parse(
+            scripts
+                .map((src) => src.innerHTML)
+                .filter((el) => el.includes("\"scorecard\""))[0] // selecting the desired <script>
+                .trim(),
+        ));
+        console.log(JSON.stringify(pageData.props.pageProps));
+        const miniScorecard = pageData.props.pageProps.miniScorecard;
+        if (miniScorecard.status) {
+            result.team1Name = miniScorecard.data.team_a.name;
+            result.team2Name = miniScorecard.data.team_b.name;
+
+            if (miniScorecard.data.team_b.innings[0].inning_start_time) {
+                result.overNum = miniScorecard.data.team_b.innings[0].overs_played;
+                result.score = miniScorecard.data.team_b.innings[0].summary.score;
+                result.crr = miniScorecard.data.team_b.innings[0].summary.rr;
+
+                result.reqrr = miniScorecard.data.match_summary.rrr;
+                result.matchEndedStr = "need" + miniScorecard.data.match_summary.ticker_summary.split("require")[1];
+                result.isSecondInning = true;
             } else {
-                data = {};
-                data.name = d.split(" ")[0];
+                result.overNum = miniScorecard.data.team_a.innings[0].overs_played;
+                result.score = miniScorecard.data.team_a.innings[0].summary.score;
+                result.crr = miniScorecard.data.team_a.innings[0].summary.rr;
             }
-            i += 1;
-        });
-    } catch (error) {
-        result.error = true;
-        console.log(error);
-    }
-
-    try {
-        const bowlersData = await page.$$eval(".end-of-over .bowl-score span", (elements) => elements.map((e) => e.textContent).slice(0, 2));
-        let i = 1;
-        let data = {};
-        bowlersData.forEach((d) => {
-            if (i % 2 === 0) {
-                data.score = d;
-                result.bowlers.push(data);
-            } else {
-                data = {};
-                data.name = d.split(" ")[0];
-            }
-            i += 1;
-        });
-    } catch (error) {
-        result.error = true;
-        console.log(error);
-    }
-    try {
-        result.currentBowler = await page.$$eval("table#table-propeller2 tbody .bowler-name-col", (elements) => elements.map((e) => e.textContent).slice(0, 1));
-    } catch (error) {
-        result.error = true;
-        console.log(error);
-    }
-
-    try {
-        result.recentBalls = await page.$$eval(".recent-over span", (elements) => {
-            let balls = [];
-            elements.forEach((e) => {
-                if (e.innerHTML === "|") {
-                    balls = [];
-                } else if (e.innerHTML !== "") {
-                    balls.push(e.innerHTML);
-                }
+            const strikeBowler = miniScorecard.data.bowlers.sb;
+            result.bowlers = [];
+            result.bowlers.push({
+                name: strikeBowler.name,
+                score: `${strikeBowler.overs}-${strikeBowler.maidens}-${strikeBowler.runs}-${strikeBowler.wickets}`,
             });
 
-            return balls
-                .filter((e) => e !== "lb" && e !== "wd" && e !== "b");
-            // .slice(balls.length - 6);
-        });
+            const strikeBatsmen = miniScorecard.data.batsmen.sb;
+            const nonstrikeBatsmen = miniScorecard.data.batsmen.nsb;
+            result.batters = [];
+            result.batters.push({
+                name: "*" + strikeBatsmen.name,
+                score: `${strikeBatsmen.runs}(${strikeBatsmen.balls})`,
+            });
+            result.batters.push({
+                name: nonstrikeBatsmen.name,
+                score: `${nonstrikeBatsmen.runs}(${nonstrikeBatsmen.balls})`,
+            });
+
+            if (miniScorecard.data.recent_over.includes("|")) {
+                result.recentBalls = miniScorecard.data.recent_over.split(" | ")[1].trim().split(" ");
+            } else {
+                result.recentBalls = miniScorecard.data.recent_over.split(" ");
+            }
+        } else if (pageData.props.pageProps.summaryData.status) {
+            result.team1Name = pageData.props.pageProps.summaryData.data.team_a.name;
+            result.team2Name = pageData.props.pageProps.summaryData.data.team_b.name;
+        }
     } catch (error) {
         result.error = true;
         console.log(error);
     }
 
-    try {
-        result.isSecondInning = (await page.$eval(".summaryScoreData .req .team", (el) => el.innerHTML)).indexOf("require") > -1;
-        const matchEndedStr = await page.$eval(".summaryScoreData .req .team", (el) => el.innerHTML);
+    // try {
+    //     const overNum = await page.$eval(".end-of-over .end-title.text-uppercase", (el) => el.innerHTML);
+    //     const overNumSplit = overNum.split(" ");
+    //     result.overNum = overNumSplit[overNumSplit.length - 1];
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
+    // try {
+    //     result.score = await page.$eval(".end-of-over .score", (el) => el.innerHTML);
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
+    // try {
+    //     result.crr = await page.$eval(".rr.current-rr b", (el) => el.innerHTML);
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
+    // try {
+    //     result.reqrr = await page.$eval(".rr.req-rr b", (el) => el.innerHTML);
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
 
-        result.matchEndedStr = result.isSecondInning ? matchEndedStr.replace(result.team2Name, "").replace("require", "").trim() : matchEndedStr;
-        result.matchEnded = result.matchEndedStr.indexOf("won") >= 0;
-    } catch (error) {
-        result.error = true;
-        console.log(error);
-    }
+    // try {
+    //     const battersData = await page.$$eval(".end-of-over .bat-score span", (elements) => elements.map((e) => e.textContent).slice(0, 4));
+    //     let i = 1;
+    //     let data = {};
+    //     battersData.forEach((d) => {
+    //         if (i % 2 === 0) {
+    //             data.score = d;
+    //             result.batters.push(data);
+    //         } else {
+    //             data = {};
+    //             data.name = d.split(" ")[0];
+    //         }
+    //         i += 1;
+    //     });
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
+
+    // try {
+    //     const bowlersData = await page.$$eval(".end-of-over .bowl-score span", (elements) => elements.map((e) => e.textContent).slice(0, 2));
+    //     let i = 1;
+    //     let data = {};
+    //     bowlersData.forEach((d) => {
+    //         if (i % 2 === 0) {
+    //             data.score = d;
+    //             result.bowlers.push(data);
+    //         } else {
+    //             data = {};
+    //             data.name = d.split(" ")[0];
+    //         }
+    //         i += 1;
+    //     });
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
+    // try {
+    //     result.currentBowler = await page.$$eval("table#table-propeller2 tbody .bowler-name-col", (elements) => elements.map((e) => e.textContent).slice(0, 1));
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
+
+    // try {
+    //     result.recentBalls = await page.$$eval(".recent-over span", (elements) => {
+    //         let balls = [];
+    //         elements.forEach((e) => {
+    //             if (e.innerHTML === "|") {
+    //                 balls = [];
+    //             } else if (e.innerHTML !== "") {
+    //                 balls.push(e.innerHTML);
+    //             }
+    //         });
+
+    //         return balls
+    //             .filter((e) => e !== "lb" && e !== "wd" && e !== "b");
+    //         // .slice(balls.length - 6);
+    //     });
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
+
+    // try {
+    //     result.isSecondInning = (await page.$eval(".summaryScoreData .req .team", (el) => el.innerHTML)).indexOf("require") > -1;
+    //     const matchEndedStr = await page.$eval(".summaryScoreData .req .team", (el) => el.innerHTML);
+
+    //     result.matchEndedStr = result.isSecondInning ? matchEndedStr.replace(result.team2Name, "").replace("require", "").trim() : matchEndedStr;
+    //     result.matchEnded = result.matchEndedStr.indexOf("won") >= 0;
+    // } catch (error) {
+    //     result.error = true;
+    //     console.log(error);
+    // }
 
     // Closing the Puppeteer controlled headless browser
     await browser.close();
